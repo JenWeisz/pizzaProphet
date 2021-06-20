33a6b14d06b3aa875d475f1fde5429e7c6cb0ddf
Jennifer weisz <weisz.jen@gmail.com>
2021-06-20 15:48:15 -0400

    added result to page

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const pizza = {
 //create variable for html form with document.querySelector
 //create event listener
 //pass e (event) to prevent default behaviour
-const form = document.querySelector('form');
+
 
 //random element from array 
 const randomElement = (array) => {
@@ -45,39 +45,58 @@ const randomElement = (array) => {
 	return array[randomIndex];
 }
 
+//put everything into an init function once done
+const pizzaProphetInit = () => {
 
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	// console.log('form submitted')
-// once form submits, grab valuse from selected radio inputs.
-	const sizeSelected = document.querySelector('input[name=size]:checked').value;
-	const priceSelected = document.querySelector('input[name=price]:checked').value;
-//  console.log(sizeSelected, priceSelected);
+	const form = document.querySelector('form');
 
-	const sizePizzaList = pizza[sizeSelected];
-	console.log(sizePizzaList);
+	form.addEventListener('submit', e => {
+		e.preventDefault();
+		// console.log('form submitted')
+	// once form submits, grab valuse from selected radio inputs.
+		const sizeSelected = document.querySelector('input[name=size]:checked').value;
+		const priceSelected = document.querySelector('input[name=price]:checked').value;
+	//  console.log(sizeSelected, priceSelected);
+	
+		const sizePizzaList = pizza[sizeSelected];
+		// console.log(sizePizzaList);
+	
+		//filter through array for lower prices/higher prices. pass filter pizza place (each item in list)
+		const withinPricePizzaList = sizePizzaList.filter( (pizzaPlace) => {
+			// console.log('pizza places list: ', pizzaPlace);
+			if (pizzaPlace.price === priceSelected) {
+				return true;
+			} else {
+				return false;
+			}
+	
+		});
+		console.log('within price list:', withinPricePizzaList);
+	
+		//this is the final pizza place we will reccomend by randomization after filtering
+		const finalPizzaPlace = randomElement(withinPricePizzaList);
+		// console.log('final pizza place object: ', finalPizzaPlace);
+	
+	
+		//display the mathcin pizza joint to user
+		const pizzaHTML = `
+		<h2>Go here!🍕 <span class="restaurant">${finalPizzaPlace.title}</span> </h2>
+		`
+		document.querySelector('.results').innerHTML = 
+		pizzaHTML;
+	
+	})
+}
 
-	//filter through array for lower prices/higher prices. pass filter pizza place (each item in list)
-	const withinPricePizzaList = sizePizzaList.filter( (pizzaPlace) => {
-		console.log('pizza places list: ', pizzaPlace);
-		if (pizzaPlace.price === priceSelected) {
-			return true;
-		} else {
-			return false;
-		}
 
-	});
-	console.log('within price list:', withinPricePizzaList);
-
-	//this is the final pizza place we will reccomend by randomization after filtering
-	const finalPizzaPlace = randomElement(withinPricePizzaList);
-	console.log('final pizza place object: ', finalPizzaPlace);
-
-})
-
-
-
-
+//javascript doc ready
+if (document.readyState === 'complete') {
+	//IE 8 SUPPORT
+	pizzaProphetInit();
+} else {
+	//for other modern browsers
+	document.addEventListener('DOMContentLoaded', pizzaProphetInit);
+}
 
 // once form submits, grab valuse from selected radio inputs.
 //use radio input values to look through pizza object
